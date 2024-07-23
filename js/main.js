3ea5de4ca3b90e4ddd8760a1412a49dbb112bb4e
Gabriel Baltar <55629378+gabrielbaltar@users.noreply.github.com>
2024-07-23 17:36:34 -0300

    feat: ex 1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 import { tooltip } from './tooltip.js';
 import dropDownMenu from './dropDownMenu.js';
 import initMenuMobile from './menu-mobile.js';
-//import menuMobileOutsideClick from './outsideClick.js';
+import initAnimaNumeros from './anima-numeros.js';
+
 
 tooltip();
 dropDownMenu();
 initMenuMobile();
-//menuMobileOutsideClick();
+initAnimaNumeros()
+
